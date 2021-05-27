c7dfc09dbc4563612991d0a90ffff73658b29dd2
s-weigand <s.weigand.phy@gmail.com>
2021-05-27 11:42:28 +0200

    👌 Changed post_build.py script to be a CLI tool which can prettyprint

--- utils/post_build.py
+++ utils/post_build.py
@@ -3,23 +3,43 @@
 from pathlib import Path
 
 from bs4 import BeautifulSoup
+from pygments import highlight
+from pygments.lexers.html import HtmlLexer
+from pygments.formatters.terminal import TerminalFormatter
+from yaargh import ArghParser
 
 
-deploy_dir = Path(__file__).parent.parent / "deploy"
-src_file = deploy_dir / "index2.html"
+DEPLOY_DIR = Path(__file__).parent.parent / "deploy"
+SRC_FILE = DEPLOY_DIR / "index2.html"
+DEST_FILE = DEPLOY_DIR / "index.html"
 
-public_url = r"https://s-weigand.github.io/dev-emoji-page"
 
-soup = BeautifulSoup(src_file.read_text(), features="lxml")
+def fix_index_html():
+    """Fixes reprent public url to links and rename file."""
+    public_url = r"https://s-weigand.github.io/dev-emoji-page"
 
-for meta_tag in soup.select("meta,link"):
-    if meta_tag.has_attr("content") and meta_tag["content"].startswith("/"):
-        meta_tag["content"] = f"{public_url}{meta_tag['content']}"
+    soup = BeautifulSoup(SRC_FILE.read_text(), features="lxml")
 
-    elif meta_tag.has_attr("href") and meta_tag["href"].startswith("/"):
-        meta_tag["href"] = f"{public_url}{meta_tag['href']}"
+    for meta_tag in soup.select("meta,link"):
+        if meta_tag.has_attr("content") and meta_tag["content"].startswith("/"):
+            meta_tag["content"] = f"{public_url}{meta_tag['content']}"
 
-dest_file = deploy_dir / "index.html"
+        elif meta_tag.has_attr("href") and meta_tag["href"].startswith("/"):
+            meta_tag["href"] = f"{public_url}{meta_tag['href']}"
 
-dest_file.write_text(str(soup))
-src_file.unlink(missing_ok=True)
+    DEST_FILE.write_text(str(soup))
+    SRC_FILE.unlink(missing_ok=True)
+
+
+def pretty_print():
+    """pretttyprint html in the terminal"""
+    pretty_html = BeautifulSoup(DEST_FILE.read_text(), features="lxml").prettify()
+    print(highlight(pretty_html, HtmlLexer(), TerminalFormatter()))
+
+
+parser = ArghParser()
+parser.add_commands([fix_index_html, pretty_print])
+
+
+if __name__ == "__main__":
+    parser.dispatch()
