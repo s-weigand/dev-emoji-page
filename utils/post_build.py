92613515f678b0627ce03708c8e5b62c376d14dc
s-weigand <s.weigand.phy@gmail.com>
2021-05-27 12:52:31 +0200

    🔧 Changed post_build.py to also fix the manifest

--- utils/post_build.py
+++ utils/post_build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 from pathlib import Path
 
 from bs4 import BeautifulSoup
@@ -12,23 +13,32 @@ from yaargh import ArghParser
 DEPLOY_DIR = Path(__file__).parent.parent / "deploy"
 SRC_FILE = DEPLOY_DIR / "index2.html"
 DEST_FILE = DEPLOY_DIR / "index.html"
+MANIFEST_FILE = DEPLOY_DIR / "manifest.webmanifest"
+PUBLIC_URL = r"https://s-weigand.github.io/dev-emoji-page"
 
 
 def fix_index_html():
-    """Fixes reprent public url to links and rename file."""
-    public_url = r"https://s-weigand.github.io/dev-emoji-page"
+    """Fixes preprent public url to links and rename html file."""
 
     soup = BeautifulSoup(SRC_FILE.read_text(), features="lxml")
 
     for meta_tag in soup.select("meta,link"):
         if meta_tag.has_attr("content") and meta_tag["content"].startswith("/"):
-            meta_tag["content"] = f"{public_url}{meta_tag['content']}"
+            meta_tag["content"] = f"{PUBLIC_URL}{meta_tag['content']}"
 
         elif meta_tag.has_attr("href") and meta_tag["href"].startswith("/"):
-            meta_tag["href"] = f"{public_url}{meta_tag['href']}"
+            meta_tag["href"] = f"{PUBLIC_URL}{meta_tag['href']}"
 
     DEST_FILE.write_text(str(soup))
     SRC_FILE.unlink(missing_ok=True)
+
+
+def fix_manifest():
+    """Fixes preprent public url to links in manifest."""
+    manifest = json.loads(MANIFEST_FILE.read_text())
+    for icon in manifest["icons"]:
+        icon["src"] = f"{PUBLIC_URL}{icon['src']}"
+    MANIFEST_FILE.write_text(json.dumps(manifest))
 
 
 def pretty_print():
@@ -37,8 +47,14 @@ def pretty_print():
     print(highlight(pretty_html, HtmlLexer(), TerminalFormatter()))
 
 
+def fix_all():
+    """Run all fixes."""
+    fix_index_html()
+    fix_manifest()
+
+
 parser = ArghParser()
-parser.add_commands([fix_index_html, pretty_print])
+parser.add_commands([fix_index_html, fix_manifest, fix_all, pretty_print])
 
 
 if __name__ == "__main__":
