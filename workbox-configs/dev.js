c5b8f21b7dc31260af4c2d80c36a1b89d55273e6
s-weigand <s.weigand.phy@gmail.com>
2024-08-31 16:15:09 +0200

    🩹⚙️ Fix dev confix for workbox

--- workbox-configs/dev.js
+++ workbox-configs/dev.js
@@ -1,12 +1,12 @@
 module.exports = {
   globDirectory: "dist/",
   globPatterns: ["**/*.{js,css,html}"],
-  globIgnores: ["**/{sw,workbox*}.js"],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: "dist/sw.js",
   skipWaiting: true,
   sourcemap: true,
   mode: "development",
-  navigateFallback: "index2.html",
+  navigateFallback: "index.html",
   runtimeCaching: [{ urlPattern: "*", handler: "NetworkFirst" }],
+  maximumFileSizeToCacheInBytes: 5000000,
 };
